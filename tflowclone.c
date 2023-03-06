819e6b0d284bd326d35c31a30db64a40bcca2d78
Brandon [L] <tangbrandonk@gmail.com>
2023-03-06 14:08:44 -0800

    Added function and fixed bugs

--- tflowclone.c
+++ tflowclone.c
@@ -4,6 +4,11 @@
 
 //TCP FLOW CLONE PROJECT
 
+void packet_analyze(){
+
+}
+
+
 int main(int argc, char** argv){
     //VARS----------------------------------
     char errbuf[PCAP_ERRBUF_SIZE];
@@ -11,9 +16,11 @@ int main(int argc, char** argv){
     char* dev;
     char* net;
     char* mask;
-    //struct pcap_pkthdr header;
+    struct pcap_pkthdr header;
     struct in_addr addr;
-    //const u_char *packet;
+    struct bpf_program filter;
+    char filter_exp[] = "tcp";
+    const u_char *packet;
     bpf_u_int32 netp; 
     bpf_u_int32 maskp;
     //---------------------------------------
@@ -23,12 +30,10 @@ int main(int argc, char** argv){
         printf("%s\n", errbuf);
         exit(1);
     }
-
     printf("DEVNAME: %s\n", dev);
 
     addr.s_addr = netp;
     net = inet_ntoa(addr);//converts addr (in inet) to ascii
-
     if(net == NULL){
         perror("inet_ntoa");
         exit(1);
@@ -38,17 +43,39 @@ int main(int argc, char** argv){
 
     addr.s_addr = maskp;
     mask = inet_ntoa(addr);
-
     if(mask == NULL){
         perror("inet_ntoa");
         exit(1);
     }
-
     printf("MASK: %s\n", mask);
+    printf("Listening On Device: %s\n\n", dev);
 
-    printf("Listening On Device: %s\n", dev);
+    //--------------------------------------------
+
+    /*
+    verifying terminal input works
+    if(argc != 0){
+        printf("%s\n", argv[1]);
+    }
+    */
+
+   //promiscuous mode denoted by the 1, meaning that it captures ALL network traffic
+   handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+    if(handle == NULL){
+        fprintf(stderr, "Could not open device: %s\n", errbuf);
+        return(1);
+    }
 
 
+   if(argc == 0){
+        while(1){
+            pcap_compile(handle, &filter, filter_exp, 1, PCAP_NETMASK_UNKNOWN);
+            packet = pcap_next(handle, &header); //actually captures the packet
+            if(packet != NULL){
+                
+            }
+        }
+   }
 
 
     return(0);
