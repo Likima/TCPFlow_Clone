906c94602cd2972301847f1255ef42f3fc10c130
Brandon [L] <tangbrandonk@gmail.com>
2023-03-18 14:39:28 -0700

    Added packet_capture.h header file

---
+++ packet_capture.h
@@ -0,0 +1,17 @@
+#ifndef PACKET_CAPTURE_H
+#define PACKET_CAPTURE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pcap.h>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <net/ethernet.h> 
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <signal.h>
+#include <time.h>
+
+#endif /* PACKET_CAPTURE_H */
