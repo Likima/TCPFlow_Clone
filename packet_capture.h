f39b433805550a2bb3f2674c40e88005091cf684
Brandon [L] <tangbrandonk@gmail.com>
2023-03-18 19:02:49 -0700

    added another #include

--- packet_capture.h
+++ packet_capture.h
@@ -13,5 +13,20 @@
 #include <string.h>
 #include <signal.h>
 #include <time.h>
+#include <ctype.h>
+
+#define typename(x) _Generic((x),                                                 \
+                                                                                  \
+        _Bool: "_Bool",                  unsigned char: "unsigned char",          \
+         char: "char",                     signed char: "signed char",            \
+    short int: "short int",         unsigned short int: "unsigned short int",     \
+          int: "int",                     unsigned int: "unsigned int",           \
+     long int: "long int",           unsigned long int: "unsigned long int",      \
+long long int: "long long int", unsigned long long int: "unsigned long long int", \
+        float: "float",                         double: "double",                 \
+  long double: "long double",                   char *: "pointer to char",        \
+       void *: "pointer to void",                int *: "pointer to int",         \
+      default: "other")
+
 
 #endif /* PACKET_CAPTURE_H */
