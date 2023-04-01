c1657b92adacfab5d6f5615c71dc5f40d5bbca7b
Brandon [L] <tangbrandonk@gmail.com>
2023-04-01 14:49:43 -0700

    added a couple of functions and removed others

--- tcp_functions.h
+++ tcp_functions.h
@@ -11,14 +11,20 @@ void sigint_handler(int sig) {
 //void printInfo(const struct packetData pack, const u_char* packet, char src_ip[INET_ADDRSTRLEN], char dst_ip[INET_ADDRSTRLEN]){
 
 //}
+void decode_udp(const struct udphdr *packet){
+    printf("   |   | Source: %hu\n", packet->source);
+    printf("   |   | Destination: %hu\n", packet->dest);
+    printf("   |   | Length: %hu\n", packet->len);
+    printf("   |   | Check: %hu\n", packet->check);
+}
 
 void decode_tcp(const struct tcphdr *packet){
-    printf("   |   | Source Port: %hu\n", packet->source);
-    printf("   |   | Destination Port: %hu\n", packet->dest);
-    printf("   |   | Sequence Number: %d\n", packet->seq);
-    printf("   |   | Acknowledgement Number: %d\n", ntohl(packet->ack_seq));
-    printf("   |   | Data Offset: %hu\n", packet->doff);
-
+    printf("   |   | Source Port: %u\n", ntohs(packet->source));
+    printf("   |   | Destination Port: %u\n", ntohs(packet->dest));
+    printf("   |   | Sequence Number: %u\n", ntohl(packet->seq));
+    printf("   |   | Acknowledgement Number: %u\n", ntohl(packet->ack_seq));
+    printf("   |   | Data Offset: %d\n", packet->doff);
+    
     printf("\nFLAGS\n\n   ____________________\n");
 //                    |__________________|
     if(packet->syn == 1){
@@ -43,15 +49,44 @@ void decode_tcp(const struct tcphdr *packet){
     printf("   |__________________|\n\n");
 }
 
-void decode_ipv4(const struct ip *packet){
-    printf("   | Header Length: %d bytes\n", packet->ip_hl);
-    printf("   | Total Length: %d bytes\n", packet->ip_len);
-    printf("   | Protocol: %d\n", packet->ip_p);
+
+
+void decode_ipvx(const struct ip *packet){
+    printf("   | Header Length: %u bytes\n", packet->ip_hl);
+    printf("   | Total Length: %u bytes\n", packet->ip_len);
+    printf("   | Protocol: %u\n", packet->ip_p);
 }
 
-void decode_ipv6(const struct ip *packet){
-
+void writef(const packetData pack, char src_ip[INET6_ADDRSTRLEN], char dst_ip[INET6_ADDRSTRLEN]){
+    fprintf(fp, "time <%s>", pack.time);
+    fprintf(fp, "length <%u> bytes\n", pack.ip_header->ip_len);
+    fprintf(fp, "protocol <%u>\n", pack.ip_header->ip_p);
+    fprintf(fp, "src_ip <%s>\ndst_ip <%s>\n\n", src_ip, dst_ip);
 }
+
+void tcpwritef(const struct tcphdr *packet){
+    fprintf(fp,"\nFLAGS\n");
+    if(packet->syn == 1){
+        fprintf(fp,"   |   SYN Flag Set   |\n");
+    } else fprintf(fp,"   | SYN Flag NOT Set |\n");
+
+    if(packet->rst == 1){
+        fprintf(fp,"   |   RST Flag Set   |\n");
+    } else fprintf(fp,"   | RST Flag NOT Set |\n");
+
+    if(packet->psh == 1){
+        fprintf(fp,"   |   PSH Flag Set   |\n");
+    } else fprintf(fp,"   | PSH Flag NOT Set |\n");
+
+    if(packet->ack == 1){
+        fprintf(fp,"   |   ACK Flag Set   |\n");
+    } else fprintf(fp,"   | ACK Flag NOT Set |\n");
+
+    if(packet->urg == 1){
+        fprintf(fp,"   |   URG Flag Set   |\n");
+    } else fprintf(fp,"   | URG Flag NOT Set |\n\n");
+}
+
 /*
 void decode_ipv4(const u_char *packet){
 
