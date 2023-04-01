3ee52d5d4ee6c010b63bb2a7e596d0aa1aa3cfd3
Brandon [L] <tangbrandonk@gmail.com>
2023-04-01 14:46:12 -0700

    can now capture unencrypted http packet data on port 80 and print it to the console and write it to an xml file.

--- tflowclone.c
+++ tflowclone.c
@@ -1,41 +1,36 @@
 #include "packet_capture.h"
 #include "tcp_functions.h"
+#include "xmlwrite.h"
 
 /*TCP FLOW CLONE PROJECT
 Captures TCP packets in a loop
 Terminated by ctrl+c
---Code "h" function to display a help menu
---Make sure to get commands with -
---code -w TCPFLOW function
+--create xml file generator
+--organize git better
+--make payload human readable (non encrypted) http packets (tcp port 80)
+mac_daddr: destination address
+mac_saddr: source address 
+https://en.wikipedia.org/wiki/Transmission_Control_Protocol
+--zlip
+--sudo tcpflow port 80 and host shinyslowfinemagic.neverssl.com
+--debug tcp packet
 */
 
 int main(int argc, char* argv[]){
     char errbuf[PCAP_ERRBUF_SIZE];
     pcap_t* handle;
     char* net; char* dev; char* mask;
+    char* filter_exp = "tcp";
     struct pcap_pkthdr header;
     struct in_addr addr;
     struct bpf_program filter;
     const u_char *packet;
     bpf_u_int32 netp; bpf_u_int32 maskp;
-    cast_var usrarg;
     int promisc = 0;
-    char* argPointer;
     int len;
-    usrarg.argv = argv;
-    usrarg.argc = argc;
-
+    signal(SIGINT, sigint_handler);
     for(int x = 1; x<argc; x=x+1){
-        if(isdigit(*argv[x]) != 0){
-            if(atoi(argv[x]) == 0 || atoi(argv[x]) == 1){
-                promisc = atoi(argv[x]);
-                continue;
-            } else {
-                printf("Promiscuous denoted by 0 or 1\n");
-                exit(1);
-            }
-        }
-
+        /*
         if(strcmp(argv[x], "-w")==0){
             if(x == argc - 1){
                 printf("Invalid usage of -w, no file provided. SYNTAX: ./tflowclone -w <filename>\n");
@@ -50,12 +45,22 @@ int main(int argc, char* argv[]){
                 printf("Invalid usage of -w, file %s provided does not exist\n", *argv[x+1]);
                 exit(1);
             }
-            
+        */
+        if(strcmp(argv[x], "-p")==0){
+            promisc = 1;
+            printf("Set To Promiscuous Mode\n\n");
+            continue;
+        }       
+        else if(strcmp(argv[x], "-e")==0){
+            if(x == argc - 1){
+                printf("Invalid usage of -e, provide filter expression\n");
+                exit(1);
+            } else filter_exp = argv[x+1];
+            //fp = fopen(argv[x+1], "w+");         
             x=x+1;
-            signal(SIGINT, sigint_handler);
+            continue;
         }
     }
-
     dev = pcap_lookupdev(errbuf);
     if(dev == NULL){
         printf("%s\n", errbuf);
@@ -71,13 +76,11 @@ int main(int argc, char* argv[]){
     printf("NET: %s\n", net);
     addr.s_addr = maskp;
     mask = inet_ntoa(addr);
-    if(mask == NULL)
-    {
+    if(mask == NULL){
         perror("inet_ntoa");
         exit(1);
     }
     printf("MASK: %s\n\n", mask);
-
     printf("Listening On Device: %s\n\n", dev);
    //promiscuous mode denoted by the 1, meaning that it captures ALL network traffic
     handle = pcap_open_live(dev, BUFSIZ, promisc, 1000, errbuf);
@@ -85,84 +88,96 @@ int main(int argc, char* argv[]){
         fprintf(stderr, "Could not open device: %s\n", errbuf);
         return 1;
     }
-
-    if(pcap_compile(handle, &filter, "udp", 1, netp) == -1) {//1 denotes speed; 0 denotes size
-        fprintf(stderr, "Couldn't compile filter: %s\n", pcap_geterr(handle));
+    if(pcap_compile(handle, &filter, filter_exp, 0, netp) == -1) {//1 denotes speed; 0 denotes size
+        fprintf(stderr, "Couldn't compile filter: %s\n", pcap_geterr(handle));//filters and checks on one line
         return 1;
     }
     if(pcap_setfilter(handle, &filter) == -1) {
         fprintf(stderr, "Couldn't set filter: %s\n", pcap_geterr(handle));
         return 1;
-    }    
+    }
 
-    pcap_loop(handle, 0, packet_handler,(u_char *) &usrarg);
+    if (access("report.xml", F_OK) != 0) {
+        system(CREATE_FILE);
+    }
+    fp = fopen("report.xml", "a+");
+
+
+    fseek (fp, 0, SEEK_END);
+    if(ftell(fp) == 0){
+        fprintf (fp,"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
+        initialize();
+    }
+    pcap_loop(handle, 0, packet_handler, NULL);
+    pcap_freecode(&filter);
     pcap_close(handle);
 
     return(0);
 }
 
-void packet_handler(u_char *user, const struct pcap_pkthdr *header, const u_char *pkt_data){
+void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_char *pkt_data){
     packetData pack;
     struct tcphdr *tPacket;
-    //cast_var* args = (cast_var*) user; //passed to function as a void pointer. Converts to a usable pointer
-    //char** argv = args->argv;
-    //int argc = args->argc;
-    char src_ip[INET_ADDRSTRLEN];
-    char dst_ip[INET_ADDRSTRLEN];
-
+    struct udphdr *uPacket;
+    char src_ip[INET6_ADDRSTRLEN];
+    char dst_ip[INET6_ADDRSTRLEN];
     pack.header = header;
     pack.time = ctime((const time_t *)&header->ts.tv_sec);
     pack.ip_header = (struct ip*)(pkt_data + sizeof(struct ether_header)); //fancy way of saying 14
-
-    /*
+    //ethernet header are the bytes up to 14
+    //ip header is the next 20 bytes
+    printf("Payload Length: %d\n", header->len);
     printf("Packet Payload: ");
-    for (int i = 0; i < header->len; i++) {
+    for (int i = 0; i < header->len; i++){
         printf("%02x ", pkt_data[i]);
     }
+    
     printf("\n");
-    */
-
+    
     //checking for ip version
     if(pack.ip_header->ip_v == 4){
         printf("IPv4 Packet\n");
-        decode_ipv4(pack.ip_header);
         inet_ntop(AF_INET, &(pack.ip_header->ip_src), src_ip, INET_ADDRSTRLEN);
         inet_ntop(AF_INET, &(pack.ip_header->ip_dst), dst_ip, INET_ADDRSTRLEN);
 
     } else if(pack.ip_header->ip_v==6){
         printf("IPv6 Packet\n");
-        decode_ipv6(pack.ip_header);
         inet_ntop(AF_INET6, &(pack.ip_header->ip_src), src_ip, INET6_ADDRSTRLEN);
         inet_ntop(AF_INET6, &(pack.ip_header->ip_dst), dst_ip, INET6_ADDRSTRLEN);
 
     } else{
         printf("UNKOWN PACKET TYPE\n");//as of rn
+        exit(1);
     }
+    decode_ipvx(pack.ip_header);
 
-    //checking for tcp packet
-    if(pack.ip_header->ip_p==6){//protocol 6 means tcp packet
+    if(pack.ip_header->ip_p==6){
         printf("   |   | TCP PACKET\n");
-        tPacket = (struct tcphdr*)(pkt_data + sizeof(struct ether_header));
+        //tPacket = (struct tcphdr*)(pkt_data + sizeof(struct ether_header)+(pack.ip_header->ip_hl*4));
+        tPacket = (struct tcphdr*)(pkt_data + sizeof(struct iphdr));
+        //tPacket = (struct tcphdr *)(pkt_data + 20);
+        fprintf(fp, "%.*s\n", header->len-tPacket->doff*4, (const char*) pkt_data+tPacket->doff*4);
         decode_tcp(tPacket);
+        printf("THIS IS THE DECODED:\n");
+        //printf("%s\n", (const char*)(pkt_data+tPacket->doff*4));
+        printf( "%.*s\n", (header->len)-(tPacket->doff*4), (const char*)(pkt_data+sizeof(struct ethhdr) + pack.ip_header->ip_hl*4 + tPacket->doff*4));
+
     }
+    if(pack.ip_header->ip_p == 17 || pack.ip_header->ip_p == 128){
+        printf("   |   | UDP PACKET\n");
+        uPacket = (struct udphdr*)(pkt_data + sizeof(struct ether_header));
+        decode_udp(uPacket);
 
-
+    }
+    
     //printInfo(pack, src_ip, dst_ip);
     printf("   | Source IP: %s\n", src_ip);
     printf("   | Destination IP: %s\n", dst_ip);
     printf("   | Packet length: %d\n", pack.header->len);
     printf("   | Packet timestamp: %s\n\n", pack.time);
 
-    /*
     if(fp != NULL){
-        fprintf(fp, "---NEW PACKET---\nTIME CAPTURED: ");
-        fprintf(fp, "%s", timestamp_str);
-        fprintf(fp, "PACKET LENGTH: ");
-        fprintf(fp, "%d\n\n", header->len);
+        writef(pack, src_ip, dst_ip);
+        if(pack.ip_header->ip_p == 6) tcpwritef(tPacket);
     }
-    //write function in separate .h file to do this
-    */
-
-        //printf("Packet timestamp: %s\n", ctime((const time_t *)&header->ts.tv_sec));
-
 }
