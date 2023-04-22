75665aacd4a386110d8904cfe9e1c5725f984253
Brandon [L] <tangbrandonk@gmail.com>
2023-04-22 15:31:33 -0700

    attempted to add gzip functionality

--- packet_capture.h
+++ packet_capture.h
@@ -10,6 +10,7 @@
 #include <net/ethernet.h> 
 #include <errno.h>
 #include <sys/socket.h>
+#include <dirent.h>
 #include <string.h>
 #include <signal.h>
 #include <time.h>
@@ -20,19 +21,25 @@
 #include <netinet/udp.h>
 #include <unistd.h>
 #include <sys/utsname.h>
-
-#define CREATE_FILE "\
-#/bin/bash \n\
-touch report.xml\
-"
+#include <stdbool.h>
+#include <zlib.h>
+#include <unistd.h>
 
 void sigint_handler(int);
 void packet_handler(u_char*, const struct pcap_pkthdr*, const u_char*);
+void sigint_handler(int);
+void decode_udp(const struct udphdr*);
+void decode_tcp(const struct tcphdr*);
+void decode_ipvx(const struct ip*);
+void tcpwritef(const struct tcphdr*);
 
+#define BUFSIZE 100
 typedef unsigned char u_char;
 
 FILE* fp = NULL;
-
+FILE* ipfp = NULL;
+bool cprint = false;
+bool html = false;
 
 typedef struct{
     char* type;
